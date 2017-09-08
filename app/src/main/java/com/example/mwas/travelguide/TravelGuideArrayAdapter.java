ee7ea2b7db28bf19e91566b888f795a7fefdb312
sammwas <samson.m.njuguna@gmail.com>
2017-09-08 16:25:26 +0300

    Create a new activity called secondary activity

---
+++ app/src/main/java/com/example/mwas/travelguide/TravelGuideArrayAdapter.java
@@ -0,0 +1,8 @@
+package com.example.mwas.travelguide;
+
+/**
+ * Created by mwas on 9/8/17.
+ */
+
+public class TravelGuideArrayAdapter {
+}
--- app/src/main/java/com/example/mwas/travelguide/secondaryActivity.java
+++ app/src/main/java/com/example/mwas/travelguide/secondaryActivity.java
@@ -2,12 +2,20 @@ package com.example.mwas.travelguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class secondaryActivity extends AppCompatActivity {
+    @Bind(R.id.secondaryActivityTitle) TextView mSecondaryActivityTitle;
+    @Bind(R.id.listActivity) ListView mListActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+        ButterKnife.bind(this);
     }
 }
