e99654672ebd1e1a8abd18792531a6ee6780a7a4
sammwas <samson.m.njuguna@gmail.com>
2017-09-08 15:29:37 +0300

    Implement an intent to switch to a new activity

--- app/src/main/java/com/example/mwas/travelguide/MainActivity.java
+++ app/src/main/java/com/example/mwas/travelguide/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.mwas.travelguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
     @Bind(R.id.locationEditText) EditText mLocationEditText;
-    @Bind(R.id.findActivityButton) Button findActivityButton;
+    @Bind(R.id.findActivityButton) Button mFindActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mFindActivityButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, secondaryActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
     }
 }
