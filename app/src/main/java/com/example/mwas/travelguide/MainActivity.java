e5c539b63c4408805247f3fd0cc488875836b2fd
sammwas <samson.m.njuguna@gmail.com>
2017-09-08 15:19:15 +0300

    Implement a butterknife

--- app/src/main/java/com/example/mwas/travelguide/MainActivity.java
+++ app/src/main/java/com/example/mwas/travelguide/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.mwas.travelguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.findActivityButton) Button findActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
