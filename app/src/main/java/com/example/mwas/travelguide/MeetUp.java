68bc7fc6d611bd354ba93d8f504becd3bd302d3a
sammwas <samson.m.njuguna@gmail.com>
2017-09-16 10:43:14 +0300

    Create a constructor to pass in the json data and create meetup objects

--- app/src/main/java/com/example/mwas/travelguide/MeetUp.java
+++ app/src/main/java/com/example/mwas/travelguide/MeetUp.java
@@ -1,8 +1,43 @@
 package com.example.mwas.travelguide;
 
+import java.util.ArrayList;
+
 /**
  * Created by mwas on 9/16/17.
  */
 
 public class MeetUp {
+    private String mName;
+    private String mDescription;
+    private String mStatus;
+    private ArrayList<String> mVenue = new ArrayList<>();
+    private ArrayList<String> mGroup = new ArrayList<>();
+
+    public MeetUp(String name, String description, String status, ArrayList<String> venue,ArrayList<String> group){
+        this.mName = name;
+        this.mDescription = description;
+        this.mStatus = status;
+        this.mVenue = venue;
+        this.mGroup = group;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getStatus() {
+        return mStatus;
+    }
+
+    public ArrayList<String> getVenue() {
+        return mVenue;
+    }
+
+    public ArrayList<String> getGroup() {
+        return mGroup;
+    }
 }
