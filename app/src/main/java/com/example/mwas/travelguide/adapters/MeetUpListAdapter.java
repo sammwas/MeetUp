11eb6796c1bde316a39de6996b2ec118acf17c23
sammwas <samson.m.njuguna@gmail.com>
2017-09-18 19:29:17 +0300

    Add the view holder to the adapter class

--- app/src/main/java/com/example/mwas/travelguide/adapters/MeetUpListAdapter.java
+++ app/src/main/java/com/example/mwas/travelguide/adapters/MeetUpListAdapter.java
@@ -1,8 +1,51 @@
 package com.example.mwas.travelguide.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.mwas.travelguide.R;
+import com.example.mwas.travelguide.models.MeetUp;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by mwas on 9/18/17.
  */
 
-public class MeetUpListAdapter {
+public class MeetUpListAdapter extends RecyclerView.Adapter<MeetUpListAdapter.ViewHolder>{
+    private ArrayList<MeetUp> mMeetUps = new ArrayList<>();
+    private Context mContext;
+
+    public MeetUpListAdapter(Context context, ArrayList<MeetUp> meetUps){
+        mContext = context;
+        mMeetUps = meetUps;
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        @Bind(R.id.meetUpNameTextView) TextView mMeetUpNameTextView;
+        @Bind(R.id.venueTextView) TextView mVenueTextView;
+        @Bind(R.id.venueAddressTextView) TextView mVenueAddressTextView;
+        @Bind(R.id.groupNameTextView) TextView mGroupNameTextView;
+
+        private Context mContext;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+            mContext = itemView.getContext();
+
+        }
+
+        public void bindMeetUp(MeetUp meetUp) {
+            mMeetUpNameTextView.setText(meetUp.getName());
+            mVenueTextView.setText(meetUp.getVenue());
+            mVenueAddressTextView.setText(meetUp.getVenue1());
+            mGroupNameTextView.setText(meetUp.getGroup1());
+
+        }
+    }
 }
