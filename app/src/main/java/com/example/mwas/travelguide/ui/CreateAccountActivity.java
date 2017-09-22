d43688174b97f6e5eacf74d9e4c9dd421ec1b788
sammwas <samson.m.njuguna@gmail.com>
2017-09-22 15:59:53 +0300

    Include the java class and layout for authentication

---
+++ app/src/main/java/com/example/mwas/travelguide/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.mwas.travelguide.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mwas.travelguide.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
