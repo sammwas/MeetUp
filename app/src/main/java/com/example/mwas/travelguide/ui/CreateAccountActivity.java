5d28f2e4bc5520f99a2d69dd59bf14f49fe8f960
sammwas <samson.m.njuguna@gmail.com>
2017-09-25 07:19:07 +0300

    Minor modifications needed for the app to return back to full functionality

--- app/src/main/java/com/example/mwas/travelguide/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/mwas/travelguide/ui/CreateAccountActivity.java
@@ -1,5 +1,6 @@
 package com.example.mwas.travelguide.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -24,7 +25,8 @@ import butterknife.ButterKnife;
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = CreateAccountActivity.class.getSimpleName();
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth.AuthStateListener mAuthListener;//listens for an account being
+    //succefully authenticated or un-authenticated
 
     @Bind(R.id.createUserButton) Button mCreateUserButton;
     @Bind(R.id.nameEditText) EditText mNameEditText;
@@ -66,6 +68,13 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         final String email = mEmailEditText.getText().toString().trim();
         String password = mPasswordEditText.getText().toString().trim();
         String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+//
+//        boolean validEmail = isValidEmail(email);
+//        boolean validName = isValidName(name);
+//        boolean validPassword = isValidPassword(password,confirmPassword);
+//
+//        if(!validEmail || !validName || !validPassword)
+//            return;
 
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -78,8 +87,10 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
+
                 });
     }
+
 
     private void createAuthStateListener() {
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -111,5 +122,34 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
+
+//    private boolean isValidEmail(String email) {
+//        boolean isGoodEmail =
+//                (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+//        if (!isGoodEmail) {
+//            mEmailEditText.setError("Please enter a valid email address");
+//            return false;
+//        }
+//        return isGoodEmail;
+//    }
+//
+//    private boolean isValidName(String name) {
+//        if (name.equals("")) {
+//            mNameEditText.setError("Please enter your name");
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    private boolean isValidPassword(String password, String confirmPassword) {
+//        if (password.length() < 6) {
+//            mPasswordEditText.setError("Please create a password containing at least 6 characters");
+//            return false;
+//        } else if (!password.equals(confirmPassword)) {
+//            mPasswordEditText.setError("Passwords do not match");
+//            return false;
+//        }
+//        return true;
+//    }
 }
 
--- app/src/main/java/com/example/mwas/travelguide/ui/MainActivity.java
+++ app/src/main/java/com/example/mwas/travelguide/ui/MainActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.mwas.travelguide.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -46,6 +50,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                    startActivity(intent);
         }
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_main, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_logout) {
+//            logout();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    private void logout() {
+//        FirebaseAuth.getInstance().signOut();
+//        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        startActivity(intent);
+//        finish();
+//    }
 
 }
 
