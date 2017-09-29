6f3a518b1caaa68fe208e09f1e63acdb07a41588
sammwas <samson.m.njuguna@gmail.com>
2017-09-29 16:18:27 +0300

    Add interface OnStartDragListener to listen to drag gestures

---
+++ app/src/main/java/com/example/mwas/travelguide/util/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package com.example.mwas.travelguide.util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by mwas on 9/29/17.
+ */
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
